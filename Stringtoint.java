9f4ba51ee84f0acc534d1be32074fb6eecf59742
JANANI-SUBASHINI <janani.jananir.r@gmail.com>
2017-07-13 21:08:49 +0530

    Create Stringtoint.java

---
+++ Stringtoint.java
@@ -0,0 +1,15 @@
+# gayu
+import java.io.*;
+import java.util.*;
+public class Stringtoint
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("Enter the integer");
+    String x=sc.nextLine();
+    int a=Integer.parseInt(x);
+    System.out.println(a);
+  }
+}
+          
